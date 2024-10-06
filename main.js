f4be8d4edd7f9f9b29bac1ee25dcb18fe4d87426
Arcgel <darkdusty8000@gmail.com>
2024-10-06 22:03:21 +0800

    temporary validation for testing the url and the given tip or content

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ function notneeded(){
     const tooltip = document.getElementById('Floating-Window');
     tooltip.remove();
 }
+window.onload = function(){
+    const url = window.location.href;
+    if(url.includes('youtube.com')){
+        function content(){
+            document.getElementById('Content').innerHTML = 'Working'
+        }
+        content();
+    }else if(url.includes('http://127.0.0.1:5500/main.html')){
+        function content(){
+            document.getElementById('Content').innerHTML = 'TestField'
+        }
+        content();
+    }else{
+        notneeded();
+    }
+}
